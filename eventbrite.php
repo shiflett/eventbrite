bce2f57d8ee7f30f7960a7b9e7780fc6272d0900
Chris Shiflett <chris@shiflett.org>
2011-04-19 21:36:19 -0400

    Add caching and enable by default.

--- eventbrite.php
+++ eventbrite.php
@@ -2,11 +2,14 @@
 
 class Eventbrite
 {
-    // FIXME: Implement caching.
 
     protected $baseUrl = 'https://www.eventbrite.com/xml/';
     protected $userKey;
     protected $appKey;
+
+    // Set default caching properties.
+    protected $cacheDir = '/tmp';
+    protected $cacheTimeout = 86400;
 
     public function __construct($userKey, $appKey)
     {
@@ -14,20 +17,43 @@ class Eventbrite
         $this->appKey = $appKey;
     }
 
+    public function cache($dir, $timeout)
+    {
+        $this->cacheDir = $dir;
+        $this->cacheTimeout = $timeout;
+    }
+
+    protected function request($url, $file)
+    {
+        $xml = '';
+
+        if (file_exists($file) && is_readable($file) && (time() - filemtime($file) < $this->cacheTimeout)) {
+            $xml = file_get_contents($file);
+        } else {
+            $xml = file_get_contents($url);
+            file_put_contents($file, $xml);
+        }
+
+        return $xml;
+    }
+
     public function eventGet($id)
     {
         // http://developer.eventbrite.com/doc/events/event_get/
-
         $url = "{$this->baseUrl}event_get?user_key={$this->userKey}&app_key={$this->appKey}&id={$id}";
-        return simplexml_load_file($url);
+        $file = "{$this->cacheDir}/eventbrite-event-get-{$id}";
+
+        return simplexml_load_string($this->request($url, $file));
+
     }
 
     public function eventListAttendees($id)
     {
         // http://developer.eventbrite.com/doc/events/event_list_attendees/
-
         $url = "{$this->baseUrl}event_list_attendees?user_key={$this->userKey}&app_key={$this->appKey}&id={$id}";
-        return simplexml_load_file($url);
+        $file = "{$this->cacheDir}/eventbrite-event-list-attendees-{$id}";
+
+        return simplexml_load_string($this->request($url, $file));
     }
 
 }
