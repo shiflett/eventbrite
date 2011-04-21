018b1a6b0109463afcc18e10a1191099ce75868e
Chris Shiflett <chris@shiflett.org>
2011-04-21 12:15:12 -0400

    Adding support for a few more methods.

--- eventbrite.php
+++ eventbrite.php
@@ -40,6 +40,7 @@ class Eventbrite
     public function eventGet($id)
     {
         // http://developer.eventbrite.com/doc/events/event_get/
+
         $url = "{$this->baseUrl}event_get?user_key={$this->userKey}&app_key={$this->appKey}&id={$id}";
         $file = "{$this->cacheDir}/eventbrite-event-get-{$id}";
 
@@ -50,8 +51,47 @@ class Eventbrite
     public function eventListAttendees($id)
     {
         // http://developer.eventbrite.com/doc/events/event_list_attendees/
+
+        // FIXME: Support optional arguments.
+
         $url = "{$this->baseUrl}event_list_attendees?user_key={$this->userKey}&app_key={$this->appKey}&id={$id}";
         $file = "{$this->cacheDir}/eventbrite-event-list-attendees-{$id}";
+
+        return simplexml_load_string($this->request($url, $file));
+    }
+
+    public function eventListDiscounts($id)
+    {
+        // http://developer.eventbrite.com/doc/events/event_list_discounts/
+
+        $url = "{$this->baseUrl}event_list_discounts?user_key={$this->userKey}&app_key={$this->appKey}&id={$id}";
+        $file = "{$this->cacheDir}/eventbrite-event-list-discounts-{$id}";
+
+        return simplexml_load_string($this->request($url, $file));
+    }
+
+    public function organizerListEvents($id)
+    {
+        // http://developer.eventbrite.com/doc/organizers/organizer_list_events/
+
+        $url = "{$this->baseUrl}event_list_discounts?user_key={$this->userKey}&app_key={$this->appKey}&id={$id}";
+        $file = "{$this->cacheDir}/eventbrite-organizer-list-events-{$id}";
+
+        return simplexml_load_string($this->request($url, $file));
+    }
+
+    public function userGet($id)
+    {
+        // http://developer.eventbrite.com/doc/users/user_get/
+
+        // Allow $id to be a user identifier or an email address.
+        if (!ctype_digit($id)) {
+            $url = "{$this->baseUrl}user_get?user_key={$this->userKey}&app_key={$this->appKey}&user_id={$id}";
+        } else {
+            $url = "{$this->baseUrl}user_get?user_key={$this->userKey}&app_key={$this->appKey}&email={$id}";
+        }
+
+        $file = "{$this->cacheDir}/eventbrite-user-get-{$id}";
 
         return simplexml_load_string($this->request($url, $file));
     }
